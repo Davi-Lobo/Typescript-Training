7c4b76ce11c95e2e135e4004956beb01fb5f171c
Davi Lobo <davi.lobo.gs@gmail.com>
2020-03-11 01:12:52 -0300

    Mongoose, app class and methods, hello world!

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,12 @@
 module.exports = {
     parser: '@typescript-eslint/parser',
+    env: {
+        es6: true,
+        node: true
+    },
     extends: [
         'plugin:@typescript-eslint/recommended',
+        'prettier/@typescript-eslint'
     ],
     parserOptions: {
         ecmaVersion: 2018,
--- dist/server.js
+++
@@ -1,3 +0,0 @@
-"use strict";const a = 12;
-
-console.log(a);
